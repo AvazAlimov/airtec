6b648fd48f6a91a39aaedb174c13e3abfde307ff
Asus <avazalimov95@mail.ru>
2017-10-22 17:23:09 +0500

    Avaz Committed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumbotron text-center" style="margin: 0">
+    <div class="jumbotron text-center" style="margin: 50px 0 0 0">
         <h1>AIRTEC.UZ</h1>
         <h2>LOREM IPSUM!</h2>
         <a class="btn btn-default">СДЕЛАТЬ ЗАКАЗ</a>
