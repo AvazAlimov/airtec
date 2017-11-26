fc376b4b468196d1b68fd950afa30d2cfdde2d7a
Asus <avazalimov95@mail.ru>
2017-11-26 15:34:56 +0500

    Avaz Committed

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 @section('script')
+    <!--suppress CssUnusedSymbol -->
     <style>
         #nav1, #nav2, #nav3, #nav4, #nav5 {
             display: none;
         }
 
         .card {
+            padding: 0;
             box-shadow: 0 0 5px #AAA;
             background-color: white;
             border-radius: 2px;
@@ -15,6 +17,12 @@
             box-shadow: 0 0 10px #AAA;
         }
 
+        .image {
+            background: no-repeat center;
+            background-size: cover;
+            height: 156px;
+            margin: 0 auto 16px;
+        }
     </style>
 @endsection
 
@@ -40,8 +48,15 @@
                 <div class="col-md-4">
                     <div class="col-md-10 col-md-offset-1 card">
                         @if(!empty($product->images))
-                            <img src="{{ asset('images/'.$product->images[0]) }}">
+                            <div class="image col-md-12"
+                                 style="background-image: url({{ asset('images/'.$product->images[0]) }});"></div>
+                        @else
+                            <div class="image col-md-12"
+                                 style="background-image: url({{ asset('resources/product.svg') }});"></div>
                         @endif
+                        <h4><strong>Цена:</strong> {{ $product->price }} сум</h4>
+                        <p style="height: 132px; overflow: hidden; text-align: justify; padding: 16px;"> {{ $product->info }} </p>
+                        <a class="btn" style="margin-bottom: 16px;">Показать</a>
                     </div>
                     <br>
                 </div>
