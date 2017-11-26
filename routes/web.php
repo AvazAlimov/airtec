94349a12b63819026d9825fe4b42b88fdf585997
Asus <avazalimov95@mail.ru>
2017-11-26 12:28:49 +0500

    Avaz Committed

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,9 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', 'WebController@welcome')->name('welcome');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/comment', 'NotificationController@comment')->name('comment.create');
 
