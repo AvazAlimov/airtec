dad7b9d8e2a41eb2e609b4721ca8821a490b24da
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2017-12-25 15:54:51 +0500

    new commit

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -32,4 +32,26 @@ class WebController extends Controller
         $message->notify(new CommentNotification());
         return redirect()->route('welcome');
     }
+    public function search(Request $request){
+
+        if ($request->search != null || $request->search == "") {
+            if (substr($request->search, 0, 1) == "#") {
+                $products = Product::where('id', ltrim($request->search, '#'));
+            }else {
+                $products = Product::where(function ($query) use ($request) {
+                    $query
+                        ->whereHas('tags', function ($query) use ($request) {
+                            $query->where('name', 'LIKE', "%$request->search%");
+                        })
+                        ->orWhere('name', 'LIKE', "%$request->search%")
+                        ->orWhere('price', 'LIKE', "%$request->search%")
+                        ->orWhere('info', 'LIKE', "%$request->search%");
+                });
+            }
+            $products = $products->paginate(9);
+        }else{
+            $products = Product::paginate(9);
+        }
+        return view('catalogue')->withProducts($products);
+    }
 }
